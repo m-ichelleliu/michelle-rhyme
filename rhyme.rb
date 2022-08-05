32d2a61cb066ebfff8e5db0e53fce9d2873ab475
m-ichelleliu <michelleliu@princeton.edu>
2022-08-05 12:03:34 -0400

    Make rhyme code less java-y, organize rspec tests

--- rhyme.rb
+++ rhyme.rb
@@ -1,6 +1,5 @@
 class Rhyme 
     def initialize
-
         @start_string = "This is"
         
         # Read each individual chunk of words
@@ -28,28 +27,28 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
 
     # Print more algorithmically, since there is a pattern
     def rhyme_hard
-      print(@suffixes)
+        generate_rhyme @suffixes.reverse
    end
     
     def rhyme_random
-      print @suffixes.shuffle
+        generate_rhyme @suffixes.shuffle
     end
 
-    def print(suffixes_array)
-        # Loop through array of word chunks, printing an extra one each time
-        output = ""
-        for i in (1..suffixes_array.length) do
-            result = @start_string + " "
-            j = i
-            while j >= 1 do
-                result += suffixes_array[suffixes_array.length - j]
-                unless j == 1 then result += " " end
-                j -= 1
-            end
-            output += result + "."
-            unless i == suffixes_array.length then output += "\n" end
+    def generate_rhyme(suffixes)
+        # Loop through array of word chunks, building on the previous output
+        lines = Array.new
+        prev_line = ""
+        suffixes.each do |suffix|
+            end_of_this_line = suffix
+            unless prev_line == "" then end_of_this_line += " " end # Corner case; we want no space between last phrase and period.
+            end_of_this_line += prev_line
+            prev_line = end_of_this_line 
+            this_line = @start_string + " " + end_of_this_line + "."
+            lines.push(this_line)
         end
-
-        output
+        lines.join("\n")
     end 
 end
+
+thing = Rhyme.new
+puts thing.rhyme_hard
--- spec/rhyme_spec.rb
+++ spec/rhyme_spec.rb
@@ -5,28 +5,36 @@ describe Rhyme do
     rhyme = Rhyme.new
     answer = File.read('jack.txt')
 
-    it 'easy print returns the rhyme string' do
-        expect(rhyme.rhyme_easy).to eq(answer)
+    describe '#easy_print' do
+        it 'returns the rhyme string' do
+            expect(rhyme.rhyme_easy).to eq(answer)
+        end
     end
 
-    it 'hard print returns the rhyme string' do
-        expect(rhyme.rhyme_hard).to eq(answer)
+    describe '#hard_print' do
+        it 'returns the rhyme string' do
+            expect(rhyme.rhyme_hard).to eq(answer)
+        end
     end
 
-    it 'random rhyme all starts with starting string' do
+    describe '#random_print' do
+        it 'all starts with the starting string' do
+            lines = rhyme.rhyme_random.split("\n")
+            lines.each do |line|
+            expect(line.start_with?(rhyme.start_string)).to eq(true)
+            end
+        end
+        
+        it 'ends each line with the previous line (minus starting string)' do
         lines = rhyme.rhyme_random.split("\n")
+        prev_line = ""
         lines.each do |line|
-          expect(line.start_with?(rhyme.start_string)).to eq(true)
+            line = line.delete_prefix(rhyme.start_string)
+            expect(line.end_with?(prev_line)).to eq(true)
+            prev_line = line
+            end
         end
-    end
-    
-    it 'ends each line with the previous line (minus starting string)' do
-      lines = rhyme.rhyme_random.split("\n")
-      prev_line = ""
-      lines.each do |line|
-          line = line.delete_prefix(rhyme.start_string)
-          expect(line.end_with?(prev_line)).to eq(true)
-          prev_line = line
-        end
-    end
+
+        # Phrases match suffixes, and each used once
+    end    
 end
